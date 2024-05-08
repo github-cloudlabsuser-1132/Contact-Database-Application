b69f9d1d309329d4adece70f2b1f4df8aa02b7ff
github_cloudlabsuser_1132@cloudlabsaiuser.com <github_cloudlabsuser_1132@cloudlabsaiuser.com>
2024-05-08 15:46:23 +0000

    deploy

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -11,18 +11,26 @@ namespace CRUD_application_2.Controllers
         public ActionResult Index()
         {
             // Implement the Index method here
+
+            // Pass the data to the view
+            return View(userlist);
         }
  
         // GET: User/Details/5
         public ActionResult Details(int id)
         {
             // Implement the details method here
+            return View(userlist[id]);
+
         }
  
         // GET: User/Create
         public ActionResult Create()
         {
             //Implement the Create method here
+            userlist.Add(new User());
+            return View();
+
         }
  
         // POST: User/Create
@@ -30,6 +38,9 @@ namespace CRUD_application_2.Controllers
         public ActionResult Create(User user)
         {
             // Implement the Create method (POST) here
+            userlist.Add(user);
+            return RedirectToAction("Index");
+
         }
  
         // GET: User/Edit/5
@@ -37,6 +48,8 @@ namespace CRUD_application_2.Controllers
         {
             // This method is responsible for displaying the view to edit an existing user with the specified ID.
             // It retrieves the user from the userlist based on the provided ID and passes it to the Edit view.
+            
+            return View(userlist[id]);
         }
  
         // POST: User/Edit/5
@@ -48,19 +61,29 @@ namespace CRUD_application_2.Controllers
             // If successful, it redirects to the Index action to display the updated list of users.
             // If no user is found with the provided ID, it returns a HttpNotFoundResult.
             // If an error occurs during the process, it returns the Edit view to display any validation errors.
+            userlist[id] = user;
+            return RedirectToAction("Index");
         }
  
         // GET: User/Delete/5
         public ActionResult Delete(int id)
         {
             // Implement the Delete method here
+            userlist.RemoveAt(id);
+            return RedirectToAction("Index");
+
         }
  
         // POST: User/Delete/5
         [HttpPost]
         public ActionResult Delete(int id, FormCollection collection)
         {
+
             // Implement the Delete method (POST) here
+            userlist.RemoveAt(id);
+            return RedirectToAction("Index");
         }
     }
 }
+
+
